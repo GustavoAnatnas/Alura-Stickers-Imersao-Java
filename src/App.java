bae9efbaa1fbebd46080bc987db6e32bd876b117
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-27 15:46:03 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        URI endereco = URI.create(url)
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> responde = client.send(request, BodyHandlers.ofString()) ;
+        String body = responde.body();
+        System.out.println(body);
+    }
+}
