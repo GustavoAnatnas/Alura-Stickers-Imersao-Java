e18aa08de48a4944c7a93c124dede12c8c3b8cb0
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-27 20:39:30 -0300

    Requisição a API /MostPopularMovies

--- src/App.java
+++ src/App.java
@@ -6,7 +6,8 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        URI endereco = URI.create(url)
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> responde = client.send(request, BodyHandlers.ofString()) ;
