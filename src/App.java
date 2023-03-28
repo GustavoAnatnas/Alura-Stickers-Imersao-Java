04b0a9edc1f069ef7ddd7a8a86845a4eb4ebd152
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-27 21:52:12 -0300

    Personaliza SOUT para facilitar leitura

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Key;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,17 @@ public class App {
 
         JsonParser parserJson = new JsonParser();
         List<Map<String, String>> moviesList = parserJson.parse(body);
-        System.out.println(moviesList);
+        // System.out.println(moviesList);
+
+        for(Map<String, String> movie : moviesList){
+        System.out.println(movie.get("fullTitle"));
+        System.out.println("Rank" + ":" + movie.get("rank"));
+        System.out.println("Ano de Lançamento" + ":" + movie.get("year"));
+        System.out.println(movie.get("image"));
+        System.out.println("Elenco" + ":" + movie.get("crew"));
+        System.out.println("Avaliação Geral" + ":" + movie.get("imDbRating"));
+        System.out.println("Contagem de Avaliações" + ":" + movie.get("imDbRatingCount"));
+        so
+       }
     }
 }
