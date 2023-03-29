7dfe76f97923949434e7210990533d0fa35e8c4d
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-29 00:01:58 -0300

    Cria metodo para gerar newImg transparencia+altura

--- src/BuildStickers.java
+++ src/BuildStickers.java
@@ -9,6 +9,12 @@ public class BuildStickers {
         //leitura da imagem
         BufferedImage originalImage = ImageIO.read(new File("imagem.jpg"));
         //cria nova imagem em memória com transparencia e com tamanho novo
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        int newHeight = height + 200;
+
+        BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+        
         //copia a imagem original para nova imagem(em memoria)
         //escrever uma frase na imagem
         //criar a imagem em um novo arquivo
