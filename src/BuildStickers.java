9c0f6d1d0ce5649d1a2c5b1637360daf49ed99c4
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-28 23:51:16 -0300

    Inicia dia 2 Imersão Alura

---
+++ src/BuildStickers.java
@@ -0,0 +1,10 @@
+public class BuildStickers {
+    public void build() {
+        //leitura da imagem
+        //cria nova imagem em memória com transparencia e com tamanho novo
+        //copia a imagem original para nova imagem(em memoria)
+        //escrever uma frase na imagem
+        //criar a imagem em um novo arquivo
+        //
+    }
+}
