35060696a8d1cdf96e00d8d12c36f166b3f1dd50
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-03-28 23:55:31 -0300

    Cria metodo responsavel por leitura imagem

--- src/BuildStickers.java
+++ src/BuildStickers.java
@@ -1,6 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class BuildStickers {
-    public void build() {
+    public void build() throws IOException {
         //leitura da imagem
+        BufferedImage originalImage = ImageIO.read(new File("imagem.jpg"));
         //cria nova imagem em memória com transparencia e com tamanho novo
         //copia a imagem original para nova imagem(em memoria)
         //escrever uma frase na imagem
